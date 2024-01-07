61088c51849af57b314987b173cc07379ec353e8
hlalljie <hlalljie@gmail.com>
2024-01-07 10:32:39 -0800

    Add exercise functionality done

--- index.js
+++ index.js
@@ -52,7 +52,40 @@ app.post('/api/users', (req, res) => {
   }
 });
 
+app.use('/api/users/:_id/exercises', bodyParser.urlencoded({extended: false}));
+
 // Save exercise record
+app.post('/api/users/:_id/exercises', async (req, res) => {
+  try{
+    const foundUser = await userRecord.findById(req.params._id).exec();
+    //foundUser.exercises.push({req.params.})
+    //res.json(foundUser);
+    // Continue if date input is valid date
+    try{
+      let exerciseDate;
+      if (req.body.date == ""){
+        exerciseDate = new Date();
+      }
+      else{
+        exerciseDate = new Date(req.body.date);
+      }
+      let username = foundUser.username;
+      foundUser.exercises.push({description: req.body.description, duration: req.body.duration, date: exerciseDate});
+      foundUser.save();
+      res.json({_id: req.params._id, username: username, date: exerciseDate.toDateString(), duration: req.body.duration, description: req.body.description});
+    }
+    catch(err){
+      console.err("Date entered not valid format")
+      res.json({error: "Date entered not valid format"});
+    }
+
+  }
+  catch(err){
+    console.error(err);
+    res.send({error: "No URL found for that id"});
+  }
+});
+
 
 
 const listener = app.listen(process.env.PORT || 3000, () => {
